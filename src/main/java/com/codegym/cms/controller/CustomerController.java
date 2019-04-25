6862b5613d55508344e8f3b5d09f08c4b3844069
ducnv1999 <ducgau99@gmail.com>
2019-04-25 11:18:59 +0700

    finish delete

--- src/main/java/com/codegym/cms/controller/CustomerController.java
+++ src/main/java/com/codegym/cms/controller/CustomerController.java
@@ -4,10 +4,7 @@ import com.codegym.cms.model.Customer;
 import com.codegym.cms.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -48,16 +45,9 @@ public class CustomerController {
 
     public ModelAndView showEditForm(@PathVariable long id){
         Customer customer = customerService.findById(id);
-        if (customer != null){
             ModelAndView modelAndView = new ModelAndView("/edit");
             modelAndView.addObject("customer", customer);
             return modelAndView;
-
-        }
-        else {
-            ModelAndView modelAndView = new ModelAndView("/error.404");
-            return modelAndView;
-        }
     }
 
     @PostMapping("/edit-customer")
@@ -68,5 +58,20 @@ public class CustomerController {
         modelAndView.addObject("message", "customer updated successfully");
         return modelAndView;
     }
+    @GetMapping("/delete-customer/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id){
+        Customer customer = customerService.findById(id);
+            ModelAndView modelAndView = new ModelAndView("/delete");
+            modelAndView.addObject("customer", customer);
+            return modelAndView;
 
+    }
+
+  @PostMapping("/delete-customer")
+    public ModelAndView deleteCustomer(@RequestParam Long id){
+        ModelAndView modelAndView = new ModelAndView("/delete");
+        customerService.remove(id);
+        modelAndView.addObject("message", "delete customer success");
+        return modelAndView;
+  }
 }
--- src/main/java/com/codegym/cms/repository/impl/CustomerRepositoryImpl.java
+++ src/main/java/com/codegym/cms/repository/impl/CustomerRepositoryImpl.java
@@ -25,7 +25,7 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     @Override
     public Customer findById(Long id) {
         TypedQuery<Customer> query = em.createQuery("select c from Customer c where  c.id=:id", Customer.class);
-        query.setParameter("id", id);
+        query.setParameter("id",id);
         try {
             return query.getSingleResult();
         }catch (NoResultException e){
